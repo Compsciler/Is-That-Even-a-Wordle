6c34a22a5f145f6bea74ffd35c5be60b064c1007
Roger Wang <rogerwang2520@gmail.com>
2022-05-18 10:46:35 -0700

    Add "Is That Even a Word? le" game

--- src/components/modals/InfoModal.tsx
+++ src/components/modals/InfoModal.tsx
@@ -10,58 +10,65 @@ export const InfoModal = ({ isOpen, handleClose }: Props) => {
   return (
     <BaseModal title="How to play" isOpen={isOpen} handleClose={handleClose}>
       <p className="text-sm text-gray-500 dark:text-gray-300">
-        Guess the word in 6 tries. After each guess, the color of the tiles will
+        Guess the word in 8 tries. After each guess, the color of the tiles will
         change to show how close your guess was to the word.
       </p>
+      <br />
+      <p className="text-sm text-gray-500 dark:text-gray-300">
+        The solution can be any 5-letter word that the New York Times does <strong>not </strong>
+        include in their word list. Note that the solution is not necessarily obscure:
+        The NYT rarely use plurals ending in "s" or past tense verbs ending in "ed."
+        However, you are free to guess any valid 5-letter word.
+      </p>
 
       <div className="flex justify-center mb-1 mt-4">
         <Cell
           isRevealing={true}
           isCompleted={true}
-          value="W"
+          value="X"
           status="correct"
         />
-        <Cell value="E" />
-        <Cell value="A" />
-        <Cell value="R" />
         <Cell value="Y" />
+        <Cell value="L" />
+        <Cell value="Y" />
+        <Cell value="L" />
       </div>
       <p className="text-sm text-gray-500 dark:text-gray-300">
-        The letter W is in the word and in the correct spot.
+        The letter X is in the word and in the correct spot.
       </p>
 
       <div className="flex justify-center mb-1 mt-4">
-        <Cell value="P" />
-        <Cell value="I" />
+        <Cell value="C" />
         <Cell
           isRevealing={true}
           isCompleted={true}
-          value="L"
+          value="R"
           status="present"
         />
-        <Cell value="O" />
+        <Cell value="W" />
         <Cell value="T" />
+        <Cell value="H" />
       </div>
       <p className="text-sm text-gray-500 dark:text-gray-300">
-        The letter L is in the word but in the wrong spot.
+        The letter R is in the word but in the wrong spot.
       </p>
 
       <div className="flex justify-center mb-1 mt-4">
-        <Cell value="V" />
         <Cell value="A" />
-        <Cell value="G" />
-        <Cell isRevealing={true} isCompleted={true} value="U" status="absent" />
-        <Cell value="E" />
+        <Cell value="A" />
+        <Cell value="R" />
+        <Cell isRevealing={true} isCompleted={true} value="G" status="absent" />
+        <Cell value="H" />
       </div>
       <p className="text-sm text-gray-500 dark:text-gray-300">
-        The letter U is not in the word in any spot.
+        The letter G is not in the word in any spot.
       </p>
 
       <p className="mt-6 italic text-sm text-gray-500 dark:text-gray-300">
         This is an open source version of the word guessing game we all know and
         love -{' '}
         <a
-          href="https://github.com/Compsciler/Wordle-With-Score-Database/"
+          href="https://github.com/Compsciler/Is-That-Even-A-Wordle"
           className="underline font-bold"
         >
           check out the code here
--- wordlistgenerator/main.py
+++ wordlistgenerator/main.py
@@ -4,15 +4,18 @@ from jsonfilelist import json_file_to_list, list_to_json_file
 
 random.seed(0)
 
-old_word_list_json_file = 'constants/oldwordlist.json'
+old_word_list_json_file = 'constants/nytwordlist.json'
 new_word_list_file = 'constants/wordlist.json'
 
 old_valid_guesses_json_file = 'constants/oldvalidGuesses.json'
 new_valid_guesses_file = 'constants/validGuesses.json'
 
 def get_word_list(old_word_list):
+    old_valid_guesses = json_file_to_list(old_valid_guesses_json_file)
     new_word_list = []
-    # TODO
+    for word in old_valid_guesses:
+        if word not in old_word_list:
+            new_word_list.append(word)
     return new_word_list
 
 old_word_list = json_file_to_list(old_word_list_json_file)
@@ -21,6 +24,5 @@ random.shuffle(new_word_list)
 list_to_json_file(new_word_list, new_word_list_file)
 
 old_valid_guesses = json_file_to_list(old_valid_guesses_json_file)
-new_valid_guesses = get_word_list(old_valid_guesses)
-new_valid_guesses.sort()
+new_valid_guesses = old_valid_guesses.copy()
 list_to_json_file(new_valid_guesses, new_valid_guesses_file)
